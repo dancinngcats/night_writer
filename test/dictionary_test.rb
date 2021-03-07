08427689d5df49a37a69945f6b1aeb644c090ff7
Kseniya K <kseniya.kudelich@gmail.com>
2021-03-06 18:27:39 -0700

    Update vocab to turn srting keys to arrays

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -12,11 +12,11 @@ class DictionaryTest < Minitest::Test
 
   def test_it_can_return_eng_to_braille_values
     assert_instance_of Hash, @dictionary.eng_to_braille
-    assert_equal "0.0.0.", @dictionary.eng_to_braille["l"]
+    assert_equal ["0.", "0.", "0."], @dictionary.eng_to_braille["l"]
   end
 
   def test_it_can_return_braille_to_end_values
     assert_instance_of Hash, @dictionary.braille_to_eng
-    assert_equal "m", @dictionary.braille_to_eng["00..0."]
+    assert_equal "l", @dictionary.braille_to_eng[["0.", "0.", "0."]]
   end
 end
