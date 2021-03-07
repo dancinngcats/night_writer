16b624dc0cf308adbae46578a38f3bc1d377a0c9
Kseniya K <kseniya.kudelich@gmail.com>
2021-03-06 23:49:51 -0700

    Add braile_array with passing test

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,22 +1,21 @@
 require_relative 'dictionary'
 
-include Dictionary
+class Translator
+  include Dictionary
 
-input = "hooray i made this woooork"
-  output = input.chars.map do |letter|
-  eng_to_braille.map do |eng, braille|
-    if letter == eng
-      braille
+  attr_reader :input_text
+
+  def initialize(input_text)
+    @input_text = input_text
+  end
+
+  def braille_array
+    input_text.chars.map do |letter|
+      eng_to_braille.map do |eng, braille|
+        if letter == eng
+          braille
+        end
+      end.compact.flatten
     end
-  end.compact.flatten
+  end
 end
-
-# add condition that if array.length == 80 then new line
-
-first_line = output.flatten.select.each_with_index { |_, i| i % 3 == 0 }
-second_line = output.flatten.select.each_with_index { |_, i| i % 3 == 1 }
-third_line = output.flatten.select.each_with_index { |_, i| i % 3 == 2 }
-
-puts first_line.reduce(:+).gsub('"', '')
-puts second_line.reduce(:+).gsub('"', '')
-puts third_line.reduce(:+).gsub('"', '')
