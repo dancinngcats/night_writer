9242134f10e99620b6efa313b04ae09c3b511f21
Kseniya K <kseniya.kudelich@gmail.com>
2021-03-07 15:45:05 -0700

    Strip character count from \n

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,4 @@
+require_relative 'translator'
 input_file = File.open(ARGV[0], "r")
 
 # input
@@ -5,14 +6,17 @@ incoming_text = input_file.read
 
 # logic that will handle translation
 # determine which filter to use - to english or to braille?
-# this will have an output
+# this will have an output_file
+translator = Translator.new(incoming_text)
 
 output_file = File.open(ARGV[1], "w")
 
-output_file.write(incoming_text)
+output_file.write(translator.braille_it)
 
 # what the output file count of characters will be
-count = incoming_text.chars.count
+stripped_count = translator.braille_it.delete("\n")
+
+count = stripped_count.size
 
 file_name = ARGV[1]
 
