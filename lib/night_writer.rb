1a9a34dddfe7ef9a91f3951201ce7fb5300b9e85
Kseniya K <kseniya.kudelich@gmail.com>
2021-03-07 18:43:44 -0700

    Remove comments

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,19 +1,15 @@
 require_relative 'translator'
 input_file = File.open(ARGV[0], "r")
 
-# input
-incoming_text = input_file.read
+#
+incoming_text = input_file.read.chomp
 
-# logic that will handle translation
-# determine which filter to use - to english or to braille?
-# this will have an output_file
 translator = Translator.new(incoming_text)
 
 output_file = File.open(ARGV[1], "w")
 
 output_file.write(translator.braille_it)
 
-# what the output file count of characters will be
 stripped_count = translator.braille_it.delete("\n")
 
 count = stripped_count.size
