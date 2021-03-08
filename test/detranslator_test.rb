73eee0d36ea864ac1cea97c398bde0809c53360e
Kseniya K <kseniya.kudelich@gmail.com>
2021-03-08 15:20:45 -0700

    Add braille_to_eng test for Dictionary module

--- test/detranslator_test.rb
+++ test/detranslator_test.rb
@@ -12,4 +12,9 @@ class DetranslatorTest < Minitest::Test
   def test_it_exists
     assert_instance_of Detranslator, @detranslator
   end
+
+  def test_it_can_return_braille_to_eng_values
+    assert_instance_of Hash, @detranslator.braille_to_eng
+    assert_equal "l", @detranslator.braille_to_eng[["0.", "0.", "0."]]
+  end
 end
