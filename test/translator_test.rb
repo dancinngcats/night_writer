079c15362d09e936deea086351f107330e6eb176
Kseniya K <kseniya.kudelich@gmail.com>
2021-03-09 00:36:25 -0700

    Refactor methods to pass arguments and update corresponding tests

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -16,40 +16,46 @@ class TranslatorTest < Minitest::Test
     assert_equal ["0.", "0.", "0."], @translator.eng_to_braille["l"]
   end
 
-  # def test_it_can_return_braille_to_end_values
-  #   assert_instance_of Hash, @translator.braille_to_eng
-  #   assert_equal "l", @translator.braille_to_eng[["0.", "0.", "0."]]
-  # end
-
   def test_it_can_return_braille_array
-    expected = [[["0.", "00", ".."]], [["0.", ".0", ".."]], [["0.", "0.", "0."]], [["0.", "0.", "0."]], [["0.", ".0", "0."]], [["..", "..", ".."]], [[".0", "00", ".0"]], [["0.", ".0", "0."]], [["0.", "00", "0."]], [["0.", "0.", "0."]], [["00", ".0", ".."]], [["..", "..", ".."]], [[".0", "0.", ".."]], [[".0", "00", "0."]], [[".0", "0.", "0."]], [["..", "..", ".."]], [["00", "..", "0."]], [["0.", ".0", ".."]]]
+    expected = [["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."], ["..", "..", ".."], [".0", "00", ".0"], ["0.", ".0", "0."], ["0.", "00", "0."], ["0.", "0.", "0."], ["00", ".0", ".."], ["..", "..", ".."], [".0", "0.", ".."], [".0", "00", "0."], [".0", "0.", "0."], ["..", "..", ".."], ["00", "..", "0."], ["0.", ".0", ".."]]
+
     assert_equal expected, @translator.braille_array
+  end
+
+  def test_it_can_only_put_80_characters_per_line
+    argument = "0.0.0.0.0....00.0.0.00...0.0.0..000.0.000.000.00.0.0.0.0....00.0.0.00...0.0.0..00"
+
+    assert_equal 81, argument.length
+
+    expected = "0.0.0.0.0....00.0.0.00...0.0.0..000.0.000.000.00.0.0.0.0....00.0.0.00...0.0.0..0"
+    assert_equal expected, @translator.slicer(argument)
   end
 
   def test_it_can_format_the_first_line
     expected = "0.0.0.0.0....00.0.0.00...0.0.0..000."
 
-    assert_equal expected, @translator.first_line
+    assert_equal expected, @translator.all_lines(0)
   end
 
   def test_it_can_format_the_second_line
     expected = "00.00.0..0..00.0000..0..0.000......0"
 
-    assert_equal expected, @translator.second_line
+    assert_equal expected, @translator.all_lines(1)
   end
 
   def test_it_can_format_the_third_line
     expected = "....0.0.0....00.0.0.......0.0...0..."
 
-    assert_equal expected, @translator.third_line
+    assert_equal expected, @translator.all_lines(2)
   end
 
   def test_it_can_return_braille_for_text_file
-    skip
-    expected =  "0.0.0.0.0....00.0.0.00...0.0.0..000.
-  00.00.0..0..00.0000..0..0.000......0
-  ....0.0.0....00.0.0.......0.0...0...
-  "
+
+    expected = "0.0.0.0.0....00.0.0.00...0.0.0..000.
+00.00.0..0..00.0000..0..0.000......0
+....0.0.0....00.0.0.......0.0...0...
+"
+
     assert_instance_of String, @translator.braille_it
     assert_equal expected, @translator.braille_it
   end
