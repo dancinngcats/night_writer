e76fb726b58f496fb2cfe24e924ed7d609f35a65
Kseniya K <kseniya.kudelich@gmail.com>
2021-03-09 00:36:34 -0700

    Refactor methods to pass arguments and update corresponding tests

--- lib/detranslator.rb
+++ lib/detranslator.rb
@@ -13,32 +13,26 @@ class Detranslator
     input_text.split()
   end
 
-  def mega_first_line
-    split_input.select.with_index{|e,i| (i+1) % 3 == 1}.join
-  end
-
-  def mega_second_line
-    split_input.select.with_index{|e,i| (i+1) % 3 == 2}.join
-  end
-
-  def mega_third_line
-    split_input.select.with_index{|e,i| (i+1) % 3 == 0}.join
+  def mega_line(leftover)
+    split_input.select.with_index{|e,i| (i+1) % 3 == (leftover)}.join
   end
 
   def mega_first_array
-    mega_first_line.chars.each_slice(2).to_a
+    mega_line(1).chars.each_slice(2).to_a
   end
 
   def mega_second_array
-    mega_second_line.chars.each_slice(2).to_a
+    mega_line(2).chars.each_slice(2).to_a
   end
 
   def mega_third_array
-    mega_third_line.chars.each_slice(2).to_a
+    mega_line(0).chars.each_slice(2).to_a
   end
 
   def rearrangement_by_index
-    mega_first_array.map.with_index { |element,index| element + mega_second_array[index] + mega_third_array[index]}
+    mega_first_array.map.with_index do |element,index|
+      element + mega_second_array[index] + mega_third_array[index]
+    end
   end
 
   def make_it_readable
@@ -46,12 +40,8 @@ class Detranslator
   end
 
   def eng_it
-    make_it_readable.map do |element|
-      braille_to_eng.map do |braille, eng|
-        if element == braille
-          eng
-        end
-      end
+    make_it_readable.map do |braille|
+      braille_to_eng[braille]
     end.join
   end
 end
